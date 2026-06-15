1c8f4f9ba9388c73493438ee0a4fb2bf90f1951d
ZimJuZiKai <julian@tom-steitz.de>
2026-06-15 09:23:09 +0200

    help

--- src/main/java/net/julian/tutorialmod/mixin/ExampleMixin.java
+++ src/main/java/net/julian/tutorialmod/mixin/ExampleMixin.java
@@ -5,7 +5,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
+//Test Commit and Push
 @Mixin(MinecraftServer.class)
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "loadWorld")
