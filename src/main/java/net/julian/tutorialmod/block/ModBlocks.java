5bda60499b99dd0dfb990483e63901412b40932f
ZimJuZiKai <julian@tom-steitz.de>
2026-06-16 15:55:27 +0200

    Episode 3, Blocks

--- src/main/java/net/julian/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/julian/tutorialmod/block/ModBlocks.java
@@ -16,30 +16,41 @@ public class ModBlocks {
 
     // --- First BLock to be added, can be copied for next ones
     public static final Block PINK_GARNET_BLOCK = registerBlock("pink_garnet_block",
-            new Block(AbstractBlock.Settings.create().strength(4f)
-                    .requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK)));
+            // --- New non-experience dropping block added, check parameters to be added by doing ...create().
+            new Block(AbstractBlock.Settings.create()
+                    // --- Block Hardness in float, no blast resistance but also float
+                    .strength(4f)
+                    // --- Tool required or broken by hand
+                    .requiresTool()
+                    // --- what sound does it play
+                    .sounds(BlockSoundGroup.AMETHYST_BLOCK)));
 
     // --- Second Block with slightly changed parameters
     public static final Block RAW_PINK_GARNET_BLOCK = registerBlock("raw_pink_garnet_block",
-            new Block(AbstractBlock.Settings.create().strength(3f)
+            new Block(AbstractBlock.Settings.create()
+                    .strength(3f)
                     .requiresTool()));
 
-    // --- Block Helper
+    // --- Block Helper , unsure how it works
     private static Block registerBlock(String name , Block block){
+        // --- registers the block, uses name and block data type (?)
         registerBlockItem(name,block);
         return Registry.register(Registries.BLOCK, Identifier.of(TutorialMod.MOD_ID, name), block);
     }
 
-    // --- Block Item Helper
+    // --- Block Item Helper , unsure how it works
     private static void registerBlockItem(String name , Block block){
+        // --- takes the same things as the above but for the blocks inventory item (?)
         Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name),
                 new BlockItem(block, new Item.Settings()));
     }
 
-    // --- Logger messages
+    // --- Block registration and Logger location, blocks are added to creative mode groups
     public static void registerModBlocks(){
+        // --- // --- Logger messages , posts custom messages on game startup, Progress update with MOD_ID
         TutorialMod.LOGGER.info("Registering Mod Blocks for " + TutorialMod.MOD_ID);
 
+        // --- added to the tab "Building Blocks"
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
             entries.add(ModBlocks.PINK_GARNET_BLOCK);
             entries.add(ModBlocks.RAW_PINK_GARNET_BLOCK);
