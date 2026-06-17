b89f722824163cdcf9cfcb9edf61d66f100ce571
ZimJuZiKai <julian@tom-steitz.de>
2026-06-17 15:42:59 +0200

    Episode 5, Recipes and Loot tables

--- src/main/java/net/julian/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/julian/tutorialmod/block/ModBlocks.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.julian.tutorialmod.TutorialMod;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -11,6 +12,7 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 public class ModBlocks {
 
@@ -20,7 +22,7 @@ public class ModBlocks {
             new Block(AbstractBlock.Settings.create()
                     // --- Block Hardness in float, no blast resistance but also float
                     .strength(4f)
-                    // --- Tool required or broken by hand
+                    // --- Tool required or broken by hand, with loot-table drops
                     .requiresTool()
                     // --- what sounds does it play
                     .sounds(BlockSoundGroup.AMETHYST_BLOCK)));
@@ -30,6 +32,21 @@ public class ModBlocks {
             new Block(AbstractBlock.Settings.create()
                     .strength(3f)
                     .requiresTool()));
+
+    // --- First Experience Dropping block, provide min and max xp and is used for ores and the likes
+    public static final Block PINK_GARNET_ORE = registerBlock("pink_garnet_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(2,5),
+                    AbstractBlock.Settings.create()
+                            .strength(3f)
+                            .requiresTool()));
+
+    // --- Second Experience dropping block, slightly changed
+    public static final Block PINK_GARNET_DEEPSLATE_ORE = registerBlock("pink_garnet_deepslate_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(3,6),
+                    AbstractBlock.Settings.create()
+                            .strength(4f)
+                            .requiresTool()
+                            .sounds(BlockSoundGroup.DEEPSLATE)));
 
     // --- Block Helper , unsure how it works
     private static Block registerBlock(String name , Block block){
